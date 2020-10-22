fb4418620febefd9aef63080bb8f48fedc5c051b
kmatsui058 <kmatsui058@gmail.com>
2020-10-22 15:32:14 +0900

    svgを読み込めるようにする

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,25 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">backlog-gantt</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <ArrowDown class="arrow-down" />
+    <img :src="ArrowLeft" alt="arrow-left" />
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
+import ArrowDown from '@/assets/images/icons/arrow-down.svg?inline'
+import ArrowLeft from '@/assets/images/icons/arrow-left.svg'
 
-@Component
-export default class IndexComponent extends Vue {}
+@Component({ components: { ArrowDown } })
+export default class IndexComponent extends Vue {
+  ArrowLeft = ArrowLeft
+}
 </script>
 
 <style lang="scss">
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.arrow-down {
+  .c {
+    stroke: red;
+  }
 }
 </style>
