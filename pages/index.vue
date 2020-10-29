6bb242596834346a442336976853bc673d67b692
kmatsui058 <kmatsui058@gmail.com>
2020-10-29 16:02:25 +0900

    認可リクエストまで

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="container">
+    <input v-model="domainName" type="text" />
+    <button @click="doLogin">login</button>
     <ControllerBox title="hoge"> hogegege </ControllerBox>
     aaaaああああ
   </div>
@@ -8,9 +10,22 @@
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 import ControllerBox from '@/components/common/ControllerBox.vue'
+import { authStore } from '~/store'
 
 @Component({ components: { ControllerBox } })
 export default class IndexComponent extends Vue {
+  get domainName(): string {
+    return authStore.getDomainName
+  }
+
+  set domainName(value: string) {
+    authStore.setDomainName(value)
+  }
+
+  doLogin(): void {
+    authStore.doLogin()
+  }
+
   created() {
     console.log(process.env.CLIENT_ID)
   }
