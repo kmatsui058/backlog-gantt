efc4467300a5f35ace92f58e9d67e2cebb7150b2
kmatsui058 <kmatsui058@gmail.com>
2020-10-29 14:59:03 +0900

    WIP

--- plugins/initializer.ts
+++ plugins/initializer.ts
@@ -6,17 +6,19 @@ const initializer: Plugin = async ({ route, app }) => {
   if (!authStore.getBacklogDomain) return
   console.log(authStore.getBacklogDomain)
   $apiConfig.basePath = authStore.getBacklogDomain
+  const code = typeof route.query.code === 'string' ? route.query.code : null
 
   if (authStore.getAccessToken) {
     $apiConfig.accessToken = authStore.getAccessToken
     await authStore.fetchSelf()
-  } else {
-    const code = typeof route.query.code === 'string' ? route.query.code : null
+  } else if (code) {
     if (code) {
       authStore.setCode(code)
       await authStore.fetchToken()
       if (app.$router) app.$router.push({ query: { code: undefined } })
     }
+  } else {
+    await authStore.fetchSelf()
   }
 }
 
