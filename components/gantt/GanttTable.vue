5b373dc9fe924c3dbc9face98131b25929e11de2
kmatsui058 <kmatsui058@gmail.com>
2020-11-29 20:15:56 +0900

    データのグルーピング

---
+++ components/gantt/GanttTable.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="gantt-table">
+    <div class="wrapper">
+      <div class="title">Gantt table</div>
+      <GanttTableLeft />
+      <GanttTableRight />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import GanttTableRight from '@/components/gantt/GanttTableRight.vue'
+import GanttTableLeft from '@/components/gantt/GanttTableLeft.vue'
+
+@Component({
+  components: { GanttTableRight, GanttTableLeft },
+})
+export default class Gantttable extends Vue {}
+</script>
--- store/gantt.ts
+++ store/gantt.ts
@@ -1,9 +1,16 @@
 import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
 import axios, { AxiosResponse, AxiosError } from 'axios'
 import qs from 'qs'
+import { Project, User } from './filter'
 import { Task } from '~/api'
 import { $apiConfig } from '~/plugins/api-accessor'
-import { dateStore, filterStore } from '~/utils/store-accessor'
+import { authStore, dateStore, filterStore } from '~/utils/store-accessor'
+
+export interface Group {
+  label: User | Project | null
+  tasks: Task[]
+}
+
 @Module({
   name: 'gantt',
   stateFactory: true,
@@ -14,6 +21,66 @@ export default class DateModule extends VuexModule {
   private response: Task[] | null = null
   get getTask(): Task[] | null {
     return this.response
+  }
+
+  get groupingTask(): Group[] {
+    if (!this.response) return []
+    const grouping = filterStore.getGrouping
+    switch (grouping) {
+      case 'none': {
+        return [
+          {
+            label: null,
+            tasks: this.response,
+          },
+        ]
+      }
+      case 'member': {
+        if (filterStore.getSelectedUserId.length) {
+          return filterStore.getSelectedUserId.map((userId) => {
+            const label =
+              filterStore.allUsers.find((user) => user.data.id === userId) ||
+              null
+            const tasks = this.response
+              ? this.response.filter((task) => task.assignee.id === userId)
+              : []
+            return { label, tasks }
+          })
+        } else {
+          return filterStore.allUsers.map((testUser) => {
+            const userId = testUser.data.id
+            const label = testUser
+            const tasks = this.response
+              ? this.response.filter((task) => task.assignee.id === userId)
+              : []
+            return { label, tasks }
+          })
+        }
+      }
+      case 'project': {
+        if (filterStore.getSelectedProjectId.length) {
+          return filterStore.getSelectedProjectId.map((projectId) => {
+            const label =
+              filterStore.getProjects.find(
+                (project) => project.data.id === projectId
+              ) || null
+            const tasks = this.response
+              ? this.response.filter((task) => task.projectId === projectId)
+              : []
+            return { label, tasks }
+          })
+        } else {
+          return filterStore.getProjects.map((project) => {
+            const projectId = project.data.id
+            const label = project
+            const tasks = this.response
+              ? this.response.filter((task) => task.projectId === projectId)
+              : []
+            return { label, tasks }
+          })
+        }
+      }
+    }
   }
 
   @Mutation
@@ -37,7 +104,7 @@ export default class DateModule extends VuexModule {
     const endDate = dateStore.getEndDate.format('YYYY-MM-DD')
     // openapi generatorのバグで配列を処理できないようなので応急処置 https://github.com/OpenAPITools/openapi-generator/issues/7966
     const res: void | AxiosResponse<Task[]> = await axios({
-      headers: { Authorization: $apiConfig.baseOptions.Authorization },
+      headers: { Authorization: `Bearer ${authStore.getAccessToken}` },
       baseURL: $apiConfig.basePath,
       method: 'get',
       url: '/api/v2/issues',
