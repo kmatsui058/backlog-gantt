6d0c40cf32a2d6fdb4b10ea254a5f1d4ae0dc15a
kmatsui058 <kmatsui058@gmail.com>
2020-11-29 21:08:32 +0900

    左側まで

--- components/gantt/GanttView.vue
+++ components/gantt/GanttView.vue
@@ -2,8 +2,13 @@
   <div class="gantt-view">
     <div class="wrapper">
       <div class="title">Gantt View</div>
-      <GanttPeriodSetter class="head" />
-      <GanttTable />
+      <client-only>
+        <GanttPeriodSetter class="head" />
+        <div v-if="!hasResult" class="loading">No Data</div>
+        <template v-for="(group, key) in groupingTask">
+          <GanttTable v-if="group.tasks.length" :key="key" :group="group" />
+        </template>
+      </client-only>
     </div>
   </div>
 </template>
@@ -12,11 +17,20 @@
 import { Vue, Component } from 'nuxt-property-decorator'
 import GanttPeriodSetter from '@/components/gantt/GanttPeriodSetter.vue'
 import GanttTable from '@/components/gantt/GanttTable.vue'
-
+import { ganttStore } from '~/store'
+import { Group } from '~/store/gantt'
 @Component({
   components: { GanttPeriodSetter, GanttTable },
 })
-export default class GanttView extends Vue {}
+export default class GanttView extends Vue {
+  get groupingTask(): Group[] {
+    return ganttStore.groupingTask || []
+  }
+
+  get hasResult(): boolean {
+    return !!ganttStore.groupingTask
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -31,5 +45,13 @@ export default class GanttView extends Vue {}
 .title {
   color: $c-navy;
   font-size: 18px;
+}
+.loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  color: $c-gray;
+  height: 200px;
 }
 </style>
