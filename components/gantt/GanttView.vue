3e2ed10be80a03e8e10dc1f7b0335e3ec28aea02
kmatsui058 <kmatsui058@gmail.com>
2020-11-29 14:42:05 +0900

    伸びるようにする

---
+++ components/gantt/GanttView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="gantt-view">
+    <div class="wrapper">
+      <div class="title">Gantt View</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({
+  components: {},
+})
+export default class GanttView extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.gantt-view {
+  padding: #{$side-space-normal}px;
+}
+.wrapper {
+  background-color: $c-white;
+  border-radius: 10px;
+  padding: 18px;
+}
+.title {
+  color: $c-navy;
+  font-size: 18px;
+}
+</style>
